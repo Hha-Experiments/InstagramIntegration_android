ccd9eb8f3fd50de4ca80a2aa18fc016623897471
Hnin Htet <hninhtet@rs-v.com>
2014-09-01 23:40:21 +0800

    constants

---
+++ instagram_integration/app/src/main/java/com/hha/instagram_integration/Constants.java
@@ -0,0 +1,19 @@
+package com.hha.instagram_integration;
+
+/**
+ * Created by HhA on 1/9/14.
+ */
+
+public class Constants {
+    public static final String BASE_URL = "https://instagram.com/";
+    public static final String API_BASE_URL = "https://api.instagram.com/";
+    public static final String AUTHENTICATE = "oauth/authorize/?response_type=token&";
+    public static final String SELF_INFO = "v1/users/self/?";
+    public static final String SELF_MEDIA = "v1/users/self/media/recent/?";
+
+    public static final String CLIENT_ID = "21325863ca004d0c96757900f51f6aee";
+    public static final String CLIENT_SECRET = "6170a88a749f4a0b9b1700d509cee8ea";
+    public static final String WEBSITE_URL = "http://localhost";
+    public static final String REDIRECT_URI = "http://localhost";
+
+}
